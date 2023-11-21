ee877d88ab7a41043013cf6a8aa7ec474d00075f
IvanGoldesov <151615150+IvanGoldesov@users.noreply.github.com>
2023-11-21 11:14:50 +0300

    add_file_via_website

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+matrix = [0, -1, -15, 13, 100, 123, 32, -23, 2, 0, 2, 2, 3, 112]
+
+def bubble_sort(matrix):
+    length = len(matrix)
+    for i in range(length-1):
+        for j in range(length-1):
+            if matrix[j] > matrix[j+1]:
+                matrix[j], matrix[j+1] = matrix[j+1], matrix[j]
